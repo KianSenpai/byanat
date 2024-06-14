bc02453bc07388a73c8efb3add4baac2b0c59ac4
Kian AliAkbari <kiana@kaytu.io>
2024-06-14 19:34:26 +0400

    add: container

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import HotelCard from './components/HotelCard'
+import Header from './components/Header'
+import Container from './components/Container'
 
 export default function App() {
     return (
-        <div className="w-screen">
-            <HotelCard
-                isLoading={false}
-                title="hello"
-                rating={3.2}
-                bedroom={2}
-                guest={3}
-                type="Entire Home"
-                bathroom={1}
-                price="10$"
-            />
-        </div>
+        <>
+            <Header />
+            <Container className="bg-slate-50 px-8 py-6">
+                <div className="grid grid-cols-5">
+                    <div className="col-span-2 flex flex-col gap-4">
+                        <HotelCard
+                            isLoading={false}
+                            title="hello"
+                            rating={3.2}
+                            bedroom={2}
+                            guest={3}
+                            type="Entire Home"
+                            bathroom={1}
+                            price="10$"
+                        />
+                        <HotelCard
+                            isLoading={false}
+                            title="hello"
+                            rating={3.2}
+                            bedroom={2}
+                            guest={3}
+                            type="Entire Home"
+                            bathroom={1}
+                            price="10$"
+                        />
+                        <HotelCard
+                            isLoading={false}
+                            title="hello"
+                            rating={3.2}
+                            bedroom={2}
+                            guest={3}
+                            type="Entire Home"
+                            bathroom={1}
+                            price="10$"
+                        />
+                    </div>
+                </div>
+            </Container>
+        </>
     )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import Container from '../Container'
+
+export default function Header() {
+    return (
+        <Container className="sticky top-0 flex items-center justify-between bg-white px-36 py-5 shadow-md">
+            hello
+        </Container>
+    )
+}
